518369b84ebc925728e065c4637fd6d03fde3a2c
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-11-15 02:30:32 -0800

    add nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,18 +3,17 @@ import Box from '@mui/material/Box';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import Button from '@mui/material/Button';
 import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 
 const Navbar = () => {
   const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
     right: false,
   });
 
@@ -38,18 +37,7 @@ const Navbar = () => {
       onKeyDown={toggleDrawer(anchor, false)}
     >
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+        {['About', 'Skills', 'Projects', 'Contact'].map((text, index) => (
           <ListItem button key={text}>
             <ListItemIcon>
               {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
@@ -62,21 +50,58 @@ const Navbar = () => {
   );
 
   return (
-    <div>
-      {['left', 'right', 'top', 'bottom'].map((anchor) => (
-        <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-          <SwipeableDrawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
-            onOpen={toggleDrawer(anchor, true)}
-          >
-            {list(anchor)}
-          </SwipeableDrawer>
-        </React.Fragment>
-      ))}
-    </div>
+    <Box
+      sx={{
+        paddingTop: '15px',
+        paddingLeft: '15px',
+        paddingRight: '15px',
+        paddingBottom: '15px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        backgroundColor: '#E2F0F9',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          paddingLeft: '5px',
+          //   justifyContent: 'center',
+          //   marginLeft: 'auto',
+        }}
+      >
+        <Typography variant='h1' sx={{ fontSize: '40px', fontWeight: 'bold' }}>
+          Coding David
+        </Typography>
+      </Box>
+      <Box
+        className='menu-btn'
+        key={'right'}
+        sx={{
+          paddingRight: '5px',
+        }}
+      >
+        <Button onClick={toggleDrawer('right', true)}>
+          <MenuOutlinedIcon
+            sx={{
+              borderRadius: '5px',
+              height: '60px',
+              width: 'auto',
+              color: 'white',
+              backgroundColor: '#286FB4',
+            }}
+          />
+        </Button>
+        <SwipeableDrawer
+          anchor={'right'}
+          open={state['right']}
+          onClose={toggleDrawer('right', false)}
+          onOpen={toggleDrawer('right', true)}
+        >
+          {list('right')}
+        </SwipeableDrawer>
+      </Box>
+    </Box>
   );
 };
 
